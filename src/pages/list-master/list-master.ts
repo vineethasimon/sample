899ff233016ea997f89baa7e817b1e3e255d5e0b
vsim9431 <35718174+vsim9431@users.noreply.github.com>
2018-03-28 00:38:46 +0530

    added changes

--- src/pages/list-master/list-master.ts
+++ src/pages/list-master/list-master.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Camera } from '@ionic-native/camera';
 import { IonicPage, ModalController, NavController,ToastController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';  
 
@@ -16,7 +16,7 @@ export class ListMasterPage {
   currentItems: Item[];
   public baseImage: string;
 
-  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController,private camera: Camera,public http: HttpClient,public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController,public camera: Camera,public http: HttpClient,public toastCtrl: ToastController) {
     this.currentItems = this.items.query();
   }
   
@@ -41,33 +41,91 @@ export class ListMasterPage {
     })
     addModal.present();
   }
+    
+    
   clickImage(){
   
-  
-          const options: CameraOptions = {
-          quality: 100,
-          destinationType: this.camera.DestinationType.DATA_URL,
-          encodingType: this.camera.EncodingType.JPEG,
-          mediaType: this.camera.MediaType.PICTURE
-          };
-  
-          this.camera.getPicture(options).then((imageData) => {
-         // imageData is either a base64 encoded string or a file URI
-         // If it's base64:
-         this.baseImage = 'data:image/jpeg;base64,' + imageData;
-//         return this.baseImage; 
-//         let toast = this.toastCtrl.create({
-//            message:"image",
-//            duration: 9000,
-//            position: 'top'
-//            });
-//            toast.present();
-        }, (err) => {
-         // Handle error
-        
+      
+      
+      
+// ss this.srcImage = '../../assets/imgs/demo.png'
+    Camera.getPicture({
+      quality: 100,
+      destinationType: 0, // DATA_URL
+      sourceType: 1,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true,
+      encodingType: Camera.EncodingType.PNG,
+      mediaType: Camera.MediaType.PICTURE
+    }).then((imageData) => {
+      /*console.log(imageData);
+      this.srcImage = `data:image/png;base64,${imageData}`;
+      setTimeout(() => {
+        (<any>window).OCRAD(document.getElementById('image'), text => {
+          alert(text);
         });
-//                                               
-//this.googleVision();
+      }, 1000);*/
+//      let loading = this.loading.create({
+//        content: 'Please wait...'
+//      });
+//    
+//      loading.present();
+  
+      const body = {
+        "requests": [
+          {
+            "image": {
+              "content": imageData
+            },
+            "features": [
+              {
+                "type": "TEXT_DETECTION"
+              }
+            ]
+          }
+        ]
+      }
+      this.http.post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDAi4IYxh4mSTyCJhkg5Opz7ZW2OBLyovI",body)
+      .subscribe(data=>{
+        console.log(data.json().response);
+        this.str=data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"");
+        loading.dismiss();
+        let alert = this.alertCtrl.create({
+          title: 'Scanned Data',
+          message: 'Please confirm scanned data',
+          inputs: [
+            {
+              name: 'title',
+              placeholder: 'Title',
+              value:data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"")
+            },
+          ],
+          buttons: [
+            {
+              text: 'Cancel',
+              handler: () => {
+                console.log('Cancel clicked');
+              }
+            },
+            {
+              text: 'Save',
+              handler: (value) => {
+                console.log('Saved clicked'+value.title);
+              }
+            }
+          ]
+        });
+        
+        alert.present();
+//        this.onInput();
+      });
+    }, (err) => {
+      console.log(`ERROR -> ${JSON.stringify(err)}`);
+    });
+
+  
+         
   
   }
   
@@ -121,7 +179,94 @@ export class ListMasterPage {
 
 
 
-  this.srcImage = '../../assets/imgs/demo.png'
+  /**
+   * Delete an item from the list of items.
+   */
+  deleteItem(item) {
+    this.items.delete(item);
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  openItem(item: Item) {
+    this.navCtrl.push('ItemDetailPage', {
+      item: item
+    });
+  }
+}
+
+
+
+\\\\
+
+import { Component } from '@angular/core';
+import { NavController, AlertController ,LoadingController} from 'ionic-angular';
+import { BarcodeScanner} from 'ionic-native';
+import { Camera } from 'ionic-native';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map'
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  text:String[];
+  srcImage: string;
+  product: any[];
+  array:any[];  
+  str:String="";
+  constructor(public navCtrl: NavController, public http: Http, public alertCtrl: AlertController,private speechRecognition: SpeechRecognition, public loading:LoadingController) { 
+    http.get('assets/json/response.json').map(res => res.json()).subscribe(data => {
+      
+          this.product = data.map(data=>data);
+          this.array = data.map(data=>data);
+        })
+  }
+   onInput(){
+
+    // if(i.data!=null){
+    //   this.str=this.str.concat(i.data)
+    // }
+    // else if(i.data==null){
+    //   this.str=this.str.slice(0,-1)
+    // }
+  //  console.log(this.str)
+   this.product=this.array.filter((event)=>{
+      if((event.product_name.toLowerCase().indexOf(this.str.toLowerCase()) > -1)||event.product_id.indexOf(this.str)>-1){
+        return true;
+      }
+      return false; 
+    })
+  }
+  async onClear(){
+    this.str="";
+    this.onInput();
+  }
+
+  clickForBarcode() {
+    BarcodeScanner.scan()
+      .then((result) => {
+        this.str=result.text;
+        alert(
+          "We got a barcode\n" +
+          "Result: " + result.text + "\n" +
+          "Format: " + result.format + "\n" +
+          "Cancelled: " + result.cancelled
+        )
+        this.onInput();
+       
+      })
+      .catch((error) => {
+        alert(error);
+      })
+  }
+  clickForOCR() {
+    this.srcImage = '../../assets/imgs/demo.png'
     Camera.getPicture({
       quality: 100,
       destinationType: 0, // DATA_URL
@@ -139,77 +284,225 @@ export class ListMasterPage {
           alert(text);
         });
       }, 1000);*/
-      let loading = this.loading.create({
-        content: 'Please wait...'
-      });
-    
-      loading.present();
-  
-      const body = {
-        "requests": [
-          {
-            "image": {
-              "content": imageData
-            },
-            "features": [
-              {
-                "type": "TEXT_DETECTION"
-              }
-            ]
-          }
-        ]
-      }
-      this.http.post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDAi4IYxh4mSTyCJhkg5Opz7ZW2OBLyovI",body)
-      .subscribe(data=>{
-        console.log(data.json().response);
-        this.str=data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"");
-        loading.dismiss();
-        let alert = this.alertCtrl.create({
-          title: 'Scanned Data',
-          message: 'Please confirm scanned data',
-          inputs: [
-            {
-              name: 'title',
-              placeholder: 'Title',
-              value:data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"")
-            },
-          ],
+        
+         let alert1 = this.alertCtrl.create({
+          title: 'Scan Image as:',
+//          message: 'Please confirm scanned data',
+//          inputs: [
+//            {
+//              name: 'title',
+//              placeholder: 'Title',
+//              value:data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"")
+//            },
+//          ],
           buttons: [
             {
-              text: 'Cancel',
-              handler: () => {
-                console.log('Cancel clicked');
-              }
+              text: 'OCR',
+              handler: (value) => {
+//                console.log('Cancel clicked');
+                  
+                   let loading = this.loading.create({
+                    content: 'Please wait...'
+                  });
+
+                  loading.present();
+
+                  const bodyTxt ={
+                    "requests": [
+                      {
+                        "image": {
+                          "content": imageData
+                        },
+                        "features": [
+                          {
+                            "type": "TEXT_DETECTION"
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                  
+                  
+                  this.http.post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDAi4IYxh4mSTyCJhkg5Opz7ZW2OBLyovI",bodyTxt)
+                  .subscribe(data=>{
+                    console.log("++++"+data.json().response);
+                    this.str=data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"");
+                    loading.dismiss();
+                    let alert = this.alertCtrl.create({
+                      title: 'Scanned Data',
+                      message: 'Please confirm scanned data',
+                      inputs: [
+                        {
+                          name: 'title',
+                          placeholder: 'Title',
+                          value:data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"")
+                        },
+                      ],
+                      buttons: [
+                        {
+                          text: 'Cancel',
+                          handler: () => {
+                            console.log('Cancel clicked');
+                          }
+                        },
+                        {
+                          text: 'Save',
+                          handler: (value) => {
+                            console.log('Saved clicked'+value.title);
+                          }
+                        }
+                      ]
+                    });
+
+                    alert.present();
+                    this.onInput();
+                  });
+
+                                 
+              } 
             },
             {
-              text: 'Save',
+              text: 'Object Image',
               handler: (value) => {
-                console.log('Saved clicked'+value.title);
+
+                  
+                        let loading = this.loading.create({
+                        content: 'Please wait...'
+                      });
+
+                      loading.present();
+
+                      const body = {
+                        "requests": [
+                          {
+                            "image": {
+                              "content": imageData
+                            },
+                            "features": [
+                              {
+                                "type": "LABEL_DETECTION",
+                                "maxResults":1
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                  
+                  
+                  this.http.post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDAi4IYxh4mSTyCJhkg5Opz7ZW2OBLyovI",body)
+                  .subscribe(data=>{
+                    console.log(data.json().response);
+                    this.str=data.json().responses["0"].labelAnnotations["0"].description;
+                    loading.dismiss();
+                    let alert = this.alertCtrl.create({
+                      title: 'Scanned Data',
+                      message: 'Please confirm scanned data',
+                      inputs: [
+                        {
+                          name: 'title',
+                          placeholder: 'Title',
+                          value: data.json().responses["0"].labelAnnotations["0"].description
+                        },
+                      ],
+                      buttons: [
+                        {
+                          text: 'Cancel',
+                          handler: () => {
+                            console.log('Cancel clicked');
+                          }
+                        },
+                        {
+                          text: 'Save',
+                          handler: (value) => {
+                            console.log('Saved clicked'+value.title);
+                          }
+                        }
+                      ]
+                    });
+
+                    alert.present();
+                    this.onInput();
+                  });
+
               }
             }
           ]
         });
+        alert1.present();
         
-        alert.present();
-        this.onInput();
-      });
+        
+//      let loading = this.loading.create({
+//        content: 'Please wait...'
+//      });
+//    
+//      loading.present();
+//  
+//      const body = {
+//        "requests": [
+//          {
+//            "image": {
+//              "content": imageData
+//            },
+//            "features": [
+//              {
+//                "type": "TEXT_DETECTION"
+//              }
+//            ]
+//          }
+//        ]
+//      }
+//      this.http.post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDAi4IYxh4mSTyCJhkg5Opz7ZW2OBLyovI",body)
+//      .subscribe(data=>{
+//        console.log(data.json().response);
+//        this.str=data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"");
+//        loading.dismiss();
+//        let alert = this.alertCtrl.create({
+//          title: 'Scanned Data',
+//          message: 'Please confirm scanned data',
+//          inputs: [
+//            {
+//              name: 'title',
+//              placeholder: 'Title',
+//              value:data.json().responses["0"].fullTextAnnotation.text.replace(/\r?\n|\r/g,"")
+//            },
+//          ],
+//          buttons: [
+//            {
+//              text: 'Cancel',
+//              handler: () => {
+//                console.log('Cancel clicked');
+//              }
+//            },
+//            {
+//              text: 'Save',
+//              handler: (value) => {
+//                console.log('Saved clicked'+value.title);
+//              }
+//            }
+//          ]
+//        });
+//        
+//        alert.present();
+//        this.onInput();
+//      });
     }, (err) => {
       console.log(`ERROR -> ${JSON.stringify(err)}`);
     });
-
-  /**
-   * Delete an item from the list of items.
-   */
-  deleteItem(item) {
-    this.items.delete(item);
   }
+  clickForSpeech(){
+    this.speechRecognition.hasPermission().then((hasPermission: boolean) => console.log(hasPermission));
+     this.speechRecognition.startListening().subscribe(data=>{
+      this.text=data;
+      alert(this.text)
+      this.str=this.text[0].replace(/ /g,"")
+      this.onInput();
+      setTimeout(() => {
+        this.speechRecognition.stopListening();
+      }, 10);
+     }
+      );
+     
+   }
 
-  /**
-   * Navigate to the detail page for this item.
-   */
-  openItem(item: Item) {
-    this.navCtrl.push('ItemDetailPage', {
-      item: item
-    });
-  }
 }
+
